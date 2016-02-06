53815076e11f749c207eba8c31eafcfe2b2e4bbb
aujerding <aujer.ding@yale.edu>
2016-02-06 02:19:57 +0000

    asd

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var express = require('express');
-<<<<<<< HEAD
-=======
-
->>>>>>> add-view-layer
 var exphbs  = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
@@ -50,10 +46,7 @@ app.use(function(req, res, next){
   }
 });
 
-<<<<<<< HEAD
-=======
 
->>>>>>> add-view-layer
 // Middleware that checks if a user is logged in. If so, the
 // request continues to be processed, otherwise a 403 is returned.
 function isLoggedIn(req, res, next){
@@ -100,10 +93,6 @@ app.get('/', loadUserTasks, function (req, res) {
 
 // Handle submitted form for new users
 app.post('/user/register', function (req, res) {
-<<<<<<< HEAD
-  if(req.body.password !== req.body.password_confirmation){
-      return res.render('index', {errors: "Password and password confirmation do not match"});
-=======
   // checks for valid registration information
   if (req.body.email === '' || req.body.fl_name === '' || req.body.password === '' || req.body.password_confirmation === '') {
     return res.render('index', {errors: "Please fill out all fields"});
@@ -127,129 +116,17 @@ app.post('/user/register', function (req, res) {
   if (!regex.test(req.body.email))
   {
     return res.render('index', {errors: "Invalid email address"});
->>>>>>> add-view-layer
   }
   if (req.body.password.length < 1) {
     err = 'Password too short';
     res.render('index', {errors: err});
     return;
   }
-<<<<<<< HEAD
-  if ( req.body.name.length < 1) {
-    return res.render('index', {errors: 'name too short'});
-  }
-
-  // Save the new user
-  var newUser = new Users();
-  newUser.hashed_password = req.body.password;
-  newUser.email = req.body.email;
-  newUser.name = req.body.fl_name;
-  newUser.save(function(err, user){
-    // If there are no errors, redirect to home page
-    if(user && !err){
-      req.session.userId = user._id;
-      res.redirect('/');
-    }
-    var errors = "Error registering you.";
-    if(err){
-      if(err.errmsg && err.errmsg.match(/duplicate/)){
-        errors = 'Account with this email already exists!';
-      }
-      return res.render('index', {errors: errors});
-    }
-  });
-});
-
-
-
-app.post('/user/login', function (req, res) {
-  // Try to find this user by email
-  Users.findOne({email: req.body.email}, function(err, user){
-
-    if(err || !user){
-      res.send('Invalid email address');
-      return;
-    }
-
-    // See if the hash of their passwords match
-    user.comparePassword(req.body.password, function(err, isMatch){
-      if(err || !isMatch){
-        res.send('Invalid password');
-      }else{
-        req.session.userId = user._id;
-        res.redirect('/');
-      }
-    });
-  });
-});
-
-// Log a user out
-app.get('/user/logout', function(req, res){
-  req.session.destroy(function(){
-    res.redirect('/');
-  });
-});
-
-//  All the controllers and routes below this require
-//  the user to be logged in.
-app.use(isLoggedIn);
-
-// Handle submission of new task form
-app.post('/task/create', function(req, res){
-  var newTask = new Tasks();
-  newTask.owner = res.locals.currentUser._id;
-  newTask.title = req.body.title;
-  newTask.description = req.body.description;
-  newTask.collaborators = [req.body.collaborator1, req.body.collaborator2, req.body.collaborator3];
-  newTask.save(function(err, savedTask){
-    if(err || !savedTask){
-      res.send('Error saving task!');
-    }else{
-      res.redirect('/');
-    }
-  });
-});
-
-app.post('/task/complete/:id', function(req,res) {
-   // console.log("jojo");
-    Tasks.findById(req.params.id, function(err,task){
-        if (err) {
-            res.send("Error finding task");
-        }else{
-            var change = false;
-            if(!task.isComplete) {
-                change = true;
-            }
-            Tasks.update({_id: req.params.id}, {isComplete: change}, function(err){
-            if(err) {
-                res.send('error updating task');
-            }else{
-                res.redirect('/');
-            }
-            
-            });
-        } 
-    });
-});
-
-// Start the server
-app.post('/task/delete/:id', function(req,res) {
-   // console.log("deleted");
-    Tasks.remove({_id: req.params.id}, function(err) {
-        if (err) {
-            res.send('Error deleting task');
-        }else {
-            res.redirect('/');
-        }
-    });
-});
-=======
   
   
   if ( req.body.fl_name.length < 1) {
     return res.render('index', {errors: 'name too short'});
   }
->>>>>>> add-view-layer
 
   // Save the new user
   var newUser = new Users();
@@ -272,10 +149,6 @@ app.post('/task/delete/:id', function(req,res) {
   });
 });
 
-<<<<<<< HEAD
-app.listen(process.env.PORT || 8080, function () {
-  
-=======
 
 
 app.post('/user/login', function (req, res) {
@@ -327,7 +200,6 @@ app.post('/task/create', function(req, res){
 });
 
 app.post('/task/complete/:id', function(req,res) {
-   // console.log("jojo");
     Tasks.findById(req.params.id, function(err,task){
         if (err) {
             res.send("Error finding task");
@@ -361,8 +233,7 @@ app.post('/task/delete/:id', function(req,res) {
 });
 
 
-app.listen(process.env.PORT, function () {
->>>>>>> add-view-layer
+app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port ' + process.env.PORT);
   
 });
